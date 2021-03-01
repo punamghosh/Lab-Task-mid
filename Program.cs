77215047149c2f117b26f47c7614a3d89f8e3520
punamghosh <78008642+punamghosh@users.noreply.github.com>
2021-03-01 17:11:54 +0600

    Add files via upload
    Committed

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Association
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Department d1 = new Department("Computer Science and Engineering","CSE");
+            Student S1 = new Student("Punam Ghosh", "11111", 3.50f);
+            S1.showDetails();
+        }
+    }
+}
